996c9f54195d2e242df33897d279d8d51f1bcf37
D-EndZ <de12@kent.ac.uk>
2026-02-10 13:37:34 +0000

    change1

--- part_2.py
+++ part_2.py
@@ -1,1 +1,15 @@
+def main():
+    rover_status = {
+    "Battery":100,
+    "Heater":"Off",
+    "Camera":"Standby"
+    }
+
+    print(rover_status)
+
+    rover_status.update({"Battery":85, "Speed":5})
+    print(rover_status)
+
+
+main()
 
