d16313f8d52cc791b69f3a7baeeaf23f88729591
D-EndZ <de12@kent.ac.uk>
2026-02-10 12:55:46 +0000

    save5

--- part_1.py
+++ part_1.py
@@ -3,4 +3,13 @@ def main():
     for i in range(len(sample_bay)):
         print("Transmitting data for " + sample_bay[i])
 
+
+    new_findings = []
+
+    for i in range(3):
+        rock = input("Give me a rock ")
+        new_findings.append(rock)
+
+    print(new_findings)
+
 main()
